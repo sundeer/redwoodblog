9bb7fddb904e922316af21be2e3a618f258ea014
sundeer <sundeer@rhsjmm.com>
2021-03-16 11:23:17 -0400

    test deploy with railway db

--- web/src/layouts/BlogLayout/BlogLayout.js
+++ web/src/layouts/BlogLayout/BlogLayout.js
@@ -1,28 +1,35 @@
 // web/src/layouts/BlogLayout/BlogLayout.js
 
-import { Link, routes } from '@redwoodjs/router'
+import { Link, routes } from "@redwoodjs/router";
+import { useAuth } from "@redwoodjs/auth";
 
 const BlogLayout = ({ children }) => {
+  const { logIn, logOut, isAuthenticated, currentUser } = useAuth();
+
   return (
-    <>
-      <header>
-        <h1>
-          <Link to={routes.home()}>Redwood Blog</Link>
-        </h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to={routes.about()}>About</Link>
-            </li>
-            <li>
-              <Link to={routes.contact()}>Contact</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
+    <div>
+      <h1>
+        <Link to={routes.home()}>Redwood Blog</Link>
+      </h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to={routes.about()}>About</Link>
+          </li>
+          <li>
+            <Link to={routes.contact()}>Contact</Link>
+          </li>
+          <li>
+            <button onClick={isAuthenticated ? logOut : logIn}>
+              {isAuthenticated ? 'Log Out' : 'Log In'}
+            </button>
+          </li>
+          {isAuthenticated && <li>{currentUser.email}</li>}
+        </ul>
+      </nav>
       <main>{children}</main>
-    </>
+    </div>
   )
 }
 
-export default BlogLayout
+export default BlogLayout;
